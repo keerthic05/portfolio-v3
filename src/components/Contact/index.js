68338b2d3220d3b150377e1ea7e30de8005b9cd2
Keerthi C <chebroluk@gmail.com>
2025-09-14 17:18:56 -0400

    built contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,63 @@
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Loader from 'react-loaders'
+import { useEffect, useState } from 'react'
+
+const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+
+        return () => clearTimeout(timeOutId)
+    }, [])
+    
+    return (
+    <>
+        <div className='container contact-page'>
+            <div className='text-zone'> 
+                <h1>
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} 
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                    I am interested in software engineering and data analysis opportunities. I look forward to 
+                    partaking in internships where I can contribute to large, innovative projects. If you
+                    want to talk more about technology, projects, or anything else, feel free to contact me. 
+                </p>
+                <div className='contact-form'>
+                    <form>
+                        <ul>
+                            <li className='half'>
+                                <input type="text" name="name" placeholder='Name' required />
+                            </li>
+                            <li className='half'>
+                                <input type="email" name="email" placeholder='Email' required />
+                            </li>
+                            <li>
+                                <input placeholder="Subject" type="text" name="subject" required />
+                            </li>
+                            <li>
+                                <textarea placeholder="Message" name="message" required></textarea>
+                            </li>
+                            <li>
+                                <input type="submit" className="flat-button" value="SEND" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <Loader type="pacman" />
+    </>)
+}
+
+export default Contact
+
