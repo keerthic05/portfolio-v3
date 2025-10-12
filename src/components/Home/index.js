53e2bef0acdd825914ab156eab857e9b75315dd8
Keerthi C <chebroluk@gmail.com>
2025-10-12 17:03:22 -0400

    rearranged home/index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,50 +2,45 @@ import { useState, useEffect } from 'react';
 import LogoTitle from '../../assets/images/logo-k.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
-import Logo from './Logo'
+import Logo from './Logo';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['e', 'e', 'r', 't', 'h', 'i']
-    const jobArray = ['s', 't', 'u', 'd', 'e', 'n', 't']
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['e', 'e', 'r', 't', 'h', 'i'];
+    const jobArray = ['s', 't', 'u', 'd', 'e', 'n', 't'];
 
     useEffect(() => {
-        const timeOutId = setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 4000)
-
-        return () => clearTimeout(timeOutId)
-    }, [])
+        const timeoutId = setTimeout(() => setLetterClass('text-animate-hover'), 4000);
+        return () => clearTimeout(timeoutId);
+    }, []);
 
     return (
         <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>    
-                <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15} />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22} />
-                </h1>
-                <h2>Computer Science, 2027 at UMass Amherst</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <span className="logo-span">
+                            <img src={LogoTitle} alt="developer" style={{ opacity: 1 }} />
+                        </span>
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+                    </h1>
+                    <h2>Computer Science, 2027 at UMass Amherst</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
-        <Loader type="pacman" />
+            <Loader type="pacman" />
         </>
     );
-}
+};
 
-export default Home
+export default Home;
